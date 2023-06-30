a70d3966a9b6cf2ccb4840ed0b4e8b8e51675bc7
chason <chason001@126.com>
2023-06-30 17:28:44 +0800

    Upload base class02 + class03

--- base/src/main/java/com/chason/class02/_02_DeleteNode_.java
+++ base/src/main/java/com/chason/class02/_02_DeleteNode_.java
@@ -1,9 +1,46 @@
 package com.chason.class02;
 
 /**
- * 删除单链表中给定的节点
+ * 删除单链表中给定的值
  */
 public class _02_DeleteNode_ {
+
+
+    /**
+     * 删除链表中的节点可能会换头
+     * @return
+     */
+    public static Node deleteNode (Node head, int number) {
+
+        if (head == null) {
+            return head;
+        }
+
+        // 头节点的处理  处理完head
+        while (head != null) {
+            if (head.value != number) {
+                break;
+            }
+            head = head.next;
+        }
+
+        // 删除链表中的元素
+
+        Node prev = new Node(-1);
+        Node curr = head;
+
+        while (curr != null) {
+
+            if (curr.value != number) {
+                prev = curr;
+            } else {
+                prev.next = curr.next;
+            }
+            curr = curr.next;
+        }
+
+        return head;
+    }
 
 
 }
