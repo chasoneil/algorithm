effe72c84561db8591fa4deac42aa9de1be10fe7
chason <chason001@126.com>
2024-04-08 14:34:53 +0800

    update fast sort

--- algorithm2024/src/main/java/com/chason/class04/_01_快速排序/Checker.java
+++ algorithm2024/src/main/java/com/chason/class04/_01_快速排序/Checker.java
@@ -3,6 +3,8 @@ package com.chason.class04._01_快速排序;
 import com.chason.utils.ArrayUtils;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Checker {
 
     @Test
@@ -17,6 +19,52 @@ public class Checker {
         int[] arr = {1, 3, 4, 2, 5, 3};
         DutchFlag.minLeftAndEqualAndRight1(arr);
         ArrayUtils.printArray(arr);
+    }
+
+    @Test
+    public void testPartition1 () {
+        int[] arr = {1, 3, 4, 2, 5, 2};
+        int p = FastSort.partition1(arr, 0, arr.length-1);
+        System.out.println(p);
+        ArrayUtils.printArray(arr);
+    }
+
+    @Test
+    public void testPartition2 () {
+        int[] arr = {1, 3, 4, 2, 5, 3};
+        int[] ints = FastSort.partition2(arr, 0, arr.length - 1);
+        System.out.println(ints[0] + ":" + ints[1]);
+        ArrayUtils.printArray(arr);
+    }
+
+    @Test
+    public void quickSortTest () {
+
+        int testTime = 500000;
+        int maxSize = 100;      // Means size  0-100
+        int maxValue = 100;     // means value -100 - 100
+
+        boolean succeed = true;
+
+        for (int i=0; i<testTime; i++) {
+
+            int[] arr = ArrayUtils.buildRandomArray(maxValue, maxSize);
+            int[] arr1 = ArrayUtils.copyArray(arr);
+            Arrays.sort(arr);
+            // TODO your sort algorithm2024
+            // FastSort.quickSort1(arr1);
+            // FastSort.quickSort2(arr1);
+
+            FastSort.quickSort3(arr1);
+            if (!ArrayUtils.isEqual(arr, arr1)) {
+                succeed = false;
+                ArrayUtils.printArray(arr);
+                ArrayUtils.printArray(arr1);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice" : "Shit");
+
     }
 
 
--- algorithm2024/src/main/java/com/chason/class04/_01_快速排序/FastSort.java
+++ algorithm2024/src/main/java/com/chason/class04/_01_快速排序/FastSort.java
@@ -8,17 +8,62 @@ public class FastSort {
             return;
         }
 
-        partition1(arr, 0, arr.length-1);
-
+        process1(arr, 0, arr.length-1);
     }
 
-    public static void process (int[] arr, int L, int R) {
+    public static void quickSort2 (int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        process2(arr, 0, arr.length-1);
+    }
+
+    public static void quickSort3 (int[] arr) {
+
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        process3(arr, 0, arr.length-1);
+    }
+
+    public static void process3 (int[] arr, int L, int R) {
+
+        if (L >= R) {
+            return;
+        }
+
+        // [0 - (R-L)]
+        // + L -> [L - R]
+        int randomPosition = (int) (Math.random() * (R-L+1)) + L;
+        swap(arr, randomPosition, R);
+        int[] area = partition2(arr, L, R);
+        process3(arr, L, area[0]-1);
+        process3(arr, area[1], R);
+    }
+
+    // arr[L...R] 上变有序
+    public static void process1 (int[] arr, int L, int R) {
+
+        if (L >= R) {
+            return;
+        }
 
         int p = partition1(arr, L, R);
+        process1(arr, L, p-1);
+        process1(arr, p+1, R);
+    }
 
+    public static void process2 (int[] arr, int L, int R) {
 
+        if (L >= R) {
+            return;
+        }
 
-
+        int[] area = partition2(arr, L, R);
+        process2(arr, L, area[0] -1);
+        process2(arr, area[1] + 1, R);
     }
 
 
@@ -47,6 +92,36 @@ public class FastSort {
 
         swap(arr, R, less+1);
         return less+1;
+    }
+
+    // arr[L...R] -> (<arr[R] =arr[R] >arr[R])
+    public static int[] partition2 (int[] arr, int L, int R) {
+
+        if (L > R) {
+            return new int[] {-1, -1};
+        }
+
+        if (L == R) {
+            return new int[] {L, R};
+        }
+
+        int less = L-1;
+        int more = R;
+
+        int index = L;
+
+        while (index < more) {
+            if (arr[index] < arr[R]) {
+                swap(arr, index++, ++less);
+            } else if (arr[index] == arr[R]) {
+                index++;
+            } else {
+                swap(arr, index, --more);
+            }
+        }
+
+        swap(arr, R, more);
+        return new int[] {less+1, more};
     }
 
 
